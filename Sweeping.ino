c65d571b9324a7b63285b08177e24cf4cd10d265
Klas Jönsson <klas.joensson@gmail.com>
2015-07-09 11:39:27 +0200

    Starting this project

---
+++ Sweeping.ino
@@ -0,0 +1,14 @@
+/* Sweeping
+ * 
+ * This program makes the light of the diodes sweep, and use four 
+ * buttons to control the speed, pause and or invert the lights.  
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
